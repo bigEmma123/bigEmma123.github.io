9a5bf4454cc2c93b2f5d4b4f431346a7d89b7b3b
bigEmma123 <enaardayfio@gmail.com>
2017-03-01 21:20:03 -0800

    changed the structure of the logic in codegroup.js

--- js/codeGroup.js
+++ js/codeGroup.js
@@ -1,107 +1,7 @@
 $(function()
 {
-    /**Global VAriables***/
-    //user index;
-    var index=100;
     
-    var DATA;
-    //increase every time a user is added
-    var num_users=12;
-    /******************/
-    //get data from the ajax request callback
-    function getData(data)
-    {
-        DATA=data;
-    }
-    var userData;
-    //getUser data
-
-    //retrieve JSON
-    /****************************************/
-    $.ajax(
-    {url:"https://bigemma123.github.io/qwajdnjsbdys/sdsk2121.json",
-	async:false,
-    global:true,
-    type:"GET",
-	dataType:"JSON",
-    complete:function(data){
-        getData(data);
-    }
-    
-    });
-    
-     function constructPage()
-    {
-        //animate the page-header again
-        //shift-header to right side
-        $('pre').show();
-        $('.code-heading').show();
-        $('.head-text').animate({fontSize:'0px'},500,function(){
-            $(this).hide();
-            $(this).css('text-align','left');
-            $(this).css('font-size','16pt');
-            $(this).delay(800).fadeIn();
-        });
-        //show codeBox2
-        $('.codeBox2').delay(500).fadeIn();
-        //animate the download btn
-        $('.download-btn').delay(500).fadeIn();
-        //animate nav
-        $('.nav').delay(500).slideDown();
-        //$('.codeBox1').load('codes/linear.txt');
-        //animate and show code boxes
-        $('.codeBox1').delay(600).fadeIn();
-        
-        
-    }
-    /*****************************************/
-    /*****************************************/
-    //get JSON 
-     var JsonData=DATA.responseText;
-    //parse the JSON Data and return string
-    var jsObject=JSON.parse(JsonData);
-    
-    
-    function authUser(mobNumber)
-    {
-        //linear search
-        for (var i=0; i<num_users; i++)
-            {
-                if (jsObject[i].Number==mobNumber)
-                    {
-                        index=i;
-                        //return true;
-                    }
-                //else return false;
-            }
-             if(jsObject[index].Number==mobNumber)
-                 {
-                     return true;
-                 }
-    }
-    
-  
-          
-    //
-    //authenticate token
-    function authToken(token){
-    for(var i=0; i<num_users; i++)
-        {
-            if(jsObject[i].token==token)
-                {
-                    index=i;
-                }
-        }
-    
-    //cross check index users token
-    if(token==jsObject[index].token)
-        {
-            return true;
-        }
-    else return false;
-}
-    //Construct login page
-    function constructLogin()
+       function constructLogin()
 {
     //hide code head
      $('.code-heading').hide();
@@ -144,43 +44,130 @@ $(function()
     //animate inputbox
     $('.form-control').animate({width:'300px'},'500',function(){
         //after animation is done put a zero in input box
-        $(this).val(0);
+        $(this).val("0");
     });    
 }
-    /*$('.form-control').keypress(function(){
-        //get input value size
-        var inputted=$('.form-control').val();
-        inputted=inputted.length;
-        console.log(inputted);
-        if (inputted==9)
-            {
-                //send an alert
-                $('.text2').addClass('alert alert-info');
-            
-                $('.text2').show();
-                
-                
-            }
-        else 
-            $('.text2').hide();
+        function constructPage()
+    {
+        //animate the page-header again
+        //shift-header to right side
+        $('pre').show();
+        $('.code-heading').show();
+        $('.head-text').animate({fontSize:'0px'},500,function(){
+            $(this).hide();
+            $(this).css('text-align','left');
+            $(this).css('font-size','16pt');
+            $(this).delay(800).fadeIn();
+        });
+        //show codeBox2
+        $('.codeBox2').delay(500).fadeIn();
+        //animate the download btn
+        $('.download-btn').delay(500).fadeIn();
+        //animate nav
+        $('.nav').delay(500).slideDown();
+        //$('.codeBox1').load('codes/linear.txt');
+        //animate and show code boxes
+        $('.codeBox1').delay(600).fadeIn();
         
-    });*/
-    //on focus
-   /* $('.form-control').mouseover(function(){
-        $('.text2').html("Please press <span class='btn authenticate btn-default'> Login <span class='glyphicon glyphicon-log-in'></span></span>  to login");
-    });*/
-    //on offload set logged in to 0
-    //add print functionality to every code
-     /********check coookie*********/
+        
+    }
+    
+   
+    
+    /**Global VAriables***/
+    //user index;
+   
+    /******************/
+    //get data from the ajax request callback
+   /* function getData(data)
+    {
+        DATA=data;
+    }*/
+    var userData;
+    //getUser data
+    var url="qwajdnjsbdys/sdsk2121.json";
+    //retrieve JSON
+    /****************************************/
+$.ajax(
+    {url:"qwajdnjsbdys/sdsk2121.json",
+    //global:true,
+    type:"GET",
+    dataType:"JSON",
+    success:function(response)
+     {
+         
+    //CLS1
+    // DATA;
+    //increase every time a user is added
+    var num_users=12;
+        
+         var jsObject=response;
+         
+    //parse the JSON Data and return string
+         
+   // var jsObject=JSON.parse(JSON.stringify(JsonData));
+         
+         /****Functons****/
+         /****1*******/
+           function authUser(mobNumber)
+    {
+        //linear search
+        //init index
+        var index=0;
+        for (var i=0; i<12; i++)
+            {
+                
+                if (jsObject[i].Number==mobNumber)
+                    {
+                        index=i;
+                        //return true;
+                    }
+                //else return false;
+            }
+        
+             if(jsObject[index].Number==mobNumber)
+                 {
+                     return true;
+                 }
+        //set token index to a hidden input's value
+        //access input index
+        $('#index').val(index);
+    }
+    /********************************************/
+         /****2*****/
+          //authenticate token
+    function authToken(token){
+        //init index
+        var index=1;
+    for(var i=0; i<num_users; i++)
+        {
+            if(jsObject[i].token==token)
+                {
+                    index=i;
+                    
+                }
+        }
+    
+    //cross check index users token
+    if(token==jsObject[index].token)
+        {
+            return true;
+        }
+    else return false;
+}
+/********************************/
+          //get JSON 
+     
+         
+         
+         
+           /********check coookie*********/
     //extract token from cookie 
     var cToken=document.cookie;
     var token=cToken.split("=");
     token=token[1];
-   
     //authenticate token
-    //var authToken=
-    console.log(authToken("454545"));
-    if(authToken(token))
+    if(authToken(token,jsObject))
         {
             //load home page
             //clear alert warning
@@ -203,10 +190,13 @@ $(function()
                 //if user chks remember me create a cookie
                 var chkVal=$('.chkBox').prop('checked');
                 //alert(chkVal);
+                //get users index to access his token 
+                var index=$('#index').val();
                 if(chkVal)
                     {
                         //set cookie
                         //use user token to set cookie
+                        
                         document.cookie="token="+jsObject[index].token;
                     }
                 else 
@@ -224,14 +214,14 @@ $(function()
         
     });
     
-    
-    //download functionality/*
-    /*$('.download_binary').click(function(){
-        $(this)[0].print();
-    });*/
+     }
+    });
 });
-//add contacts page to show email and whatsapp number
-//show animations for the search algo's
+ 
  
  
 
+/****Note********
+Had problem with the index where I had to make it local to function authuser and authtoken
+so had to user a hidden input filed with id index so that i can set index in those functions to the #index value 
+ln 197 accessed the value of #index.****/
